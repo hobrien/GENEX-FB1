386c9a87eb66d9df636029db819fee7974261920
Heath O'Brien <heath.obrien@gmail.com>
2017-04-12 17:01:39 +0100

    change output directory in command

--- Bash/Tophat2.sh
+++ Bash/Tophat2.sh
@@ -10,5 +10,5 @@
 
 tophat --keep-fasta-order --library-type fr-secondstrand --mate-inner-dist 500 --mate-std-dev 50 --num-threads 8 \
       --transcriptome-index GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.inx \
-      --output-dir $BASEDIR/$SampleID \
+      --output-dir Results \
       /GRCh38/NCBI/GRCh38Decoy/Sequence/Bowtie2Index/genome $@
