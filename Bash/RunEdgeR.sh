5696f814a7d1dc75ebe8bd324ab3d2b1fd237c75
Heath O'Brien <heath.obrien@gmail.com>
2017-05-04 13:24:40 +0100

    added analyses with low RIN samples removed

--- Bash/RunEdgeR.sh
+++ Bash/RunEdgeR.sh
@@ -12,3 +12,11 @@ Rscript FvsMedgeR.r --min 13 --max 14
 Rscript FvsMedgeR.r --min 14 --max 15
 Rscript FvsMedgeR.r --min 15 --max 17
 Rscript FvsMedgeR.r --min 17 --max 20
+
+# Rscript FvsMedgeR.r --min 12 --max 20 -a -r 5
+# Rscript FvsMedgeR.r --min 12 --max 13 -r 5
+# #Rscript FvsMedgeR.r --min 13 --max 14 -r 5
+# Rscript FvsMedgeR.r --min 14 --max 15 -r 5
+# Rscript FvsMedgeR.r --min 15 --max 17 -r 5
+# Rscript FvsMedgeR.r --min 17 --max 20 -r 5
+
