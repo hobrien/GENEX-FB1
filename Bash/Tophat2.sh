c7d3d1827e681ed3577fb7f268324a0d67f2bdb1
hobrien <heath.obrien@gmail.com>
2017-06-20 15:57:11 +0100

    reversed order of arguments because they were inconsistent with the pipeline

--- Bash/Tophat2.sh
+++ Bash/Tophat2.sh
@@ -10,5 +10,5 @@
 
 tophat --keep-fasta-order --library-type fr-secondstrand --mate-inner-dist 500 --mate-std-dev 50 --num-threads 8 \
       --transcriptome-index /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.inx \
-      --output-dir $1 \
-      /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/Bowtie2Index/genome $2 $3
+      --output-dir $3 \
+      /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/Bowtie2Index/genome $1 $2
