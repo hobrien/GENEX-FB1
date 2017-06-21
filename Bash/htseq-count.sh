b11398105f3919a3bf9fd768991643496f3cdf10
hobrien <heath.obrien@gmail.com>
2017-06-21 16:22:19 +0100

    fixed locaiton of gtf file

--- Bash/htseq-count.sh
+++ Bash/htseq-count.sh
@@ -11,7 +11,7 @@ echo "Getting counts for $1"
   
 htseq-count -f bam -s reverse -t exon -i gene_id -m intersection-strict \
   $1 \
-  GRCh38/NCBI/GRCh38Decoy/Annotation/genes.gtf \
+   /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.gtf \
   > ${1%%.bam}_counts.txt
 echo "Finished getting counts for $1"
 
