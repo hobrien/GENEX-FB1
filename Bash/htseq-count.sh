748886bf1ea3624baca19aa04328f0113a797263
hobrien <heath.obrien@gmail.com>
2017-06-28 11:26:23 +0100

    changed names of output files

--- Bash/htseq-count.sh
+++ Bash/htseq-count.sh
@@ -12,6 +12,6 @@ echo "Getting counts for $1"
 htseq-count -f bam -s reverse -t exon -i gene_id -m intersection-strict \
   $1 \
    /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.gtf \
-  > ${1%%.bam}_counts.txt
+  > ${1%%.bam}.counts.txt
 echo "Finished getting counts for $1"
 
