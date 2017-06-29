5b97ac7e9e78a0fcbdfe46d45e904d79a50a2181
Heath O'Brien <heath.obrien@gmail.com>
2017-06-29 13:18:19 +0100

    Run from main directory rather than R subdirectory

--- R/PrepareData.R
+++ R/PrepareData.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(stringr)
 
 # Oh yes, thought of a catchy (and hopefully memorable) name for the dataset: 
 # GENEX-FB (for GENe EXpression in the Fetal Brain). 
@@ -6,44 +7,48 @@ library(tidyverse)
 # The larger dataset for eQTL / TWAS analysis (I reckon we'll get up to 150) would be GENEX-FB2: 
 # Genotypic effects. (If we grew the sample, we'd call it FB3 etc). 
 # We could also use GENEX for the adult brain samples (E.g. GENEX-AC (adult caudate)!
-gene_info <- read_tsv("../Data/genes.csv") %>%
+setwd("~/BTSync/FetalRNAseq/Github/GENEX-FB1/")
+gene_info <- read_tsv("Data/genes.txt") %>%
   mutate(gene_id = sub("\\.[0-9]+", "", gene_id)) %>%
   dplyr::select(Id = gene_id, SYMBOL=gene_name, Chr=seqid)
 
-counts <- read_delim("../Results/MvsF12_19_FDR_0.1/BG12_19.txt", "\t", escape_double = FALSE, trim_ws = TRUE) %>%
-  dplyr::select(Id, starts_with('norm'))
+counts <- read_delim("Results/MvsF_12_20_PCW_FDR_0.1/tables/MalevsFemale.complete.txt", "\t", escape_double = FALSE, trim_ws = TRUE) %>%
+  mutate(Id=str_extract(Id, '^[^.]+'))
+  
 
-right_join(gene_info, counts) %>% 
-  write_tsv("../Shiny/GENEX-FB1/Data/counts.txt")
+right_join(gene_info, dplyr::select(counts, Id, starts_with('norm'))) %>% 
+  write_tsv("Shiny/GENEX-FB1/Data/counts.txt")
+right_join(gene_info, dplyr::select(counts, Id, starts_with('norm'))) %>% 
+  write_tsv("Tables/raw_counts.txt")
 
-fittedBias <- read_delim("../Results/MvsF12_19_FDR_0.1/BG12_19.txt", "\t", escape_double = FALSE, trim_ws = TRUE) %>%
+fittedBias <- read_delim("Results/MvsF_12_20_PCW_FDR_0.1/tables/BG12_20.txt", "\t", escape_double = FALSE, trim_ws = TRUE) %>%
   dplyr::select(Id, Male, Female, log2FoldChange, pvalue, padj) %>% 
   mutate(ageBin='12-19') %>%
   bind_rows(
-    read_delim("../Results/MvsF12_FDR_0.1/BG12.txt", "\t", escape_double = FALSE, trim_ws = TRUE) %>%
+    read_delim("Results/MvsF_12_FDR_0.1/tables/BG12.txt", "\t", escape_double = FALSE, trim_ws = TRUE) %>%
       dplyr::select(Id, Male, Female, log2FoldChange, pvalue, padj) %>% 
       mutate(ageBin='12')
   ) %>%
   bind_rows(
-    read_delim("../Results/MvsF13_FDR_0.1/BG13.txt", "\t", escape_double = FALSE, trim_ws = TRUE) %>%
+    read_delim("Results/MvsF_13_FDR_0.1/tables/BG13.txt", "\t", escape_double = FALSE, trim_ws = TRUE) %>%
       dplyr::select(Id, Male, Female, log2FoldChange, pvalue, padj) %>% 
       mutate(ageBin='13')
   ) %>%
   bind_rows(
-    read_delim("../Results/MvsF14_FDR_0.1/BG14.txt", "\t", escape_double = FALSE, trim_ws = TRUE) %>%
+    read_delim("Results/MvsF_14_FDR_0.1/tables/BG14.txt", "\t", escape_double = FALSE, trim_ws = TRUE) %>%
       dplyr::select(Id, Male, Female, log2FoldChange, pvalue, padj) %>% 
       mutate(ageBin='14')
   ) %>%
   bind_rows(
-    read_delim("../Results/MvsF15_16_FDR_0.1/BG15_16.txt", "\t", escape_double = FALSE, trim_ws = TRUE) %>%
+    read_delim("Results/MvsF_15_17_FDR_0.1/tables/BG15_17.txt", "\t", escape_double = FALSE, trim_ws = TRUE) %>%
       dplyr::select(Id, Male, Female, log2FoldChange, pvalue, padj) %>% 
       mutate(ageBin='15-16')
   ) %>%
   bind_rows(
-    read_delim("../Results/MvsF17_19_FDR_0.1/BG17_19.txt", "\t", escape_double = FALSE, trim_ws = TRUE) %>%
+    read_delim("Results/MvsF_17_20_FDR_0.1/tables/BG17_20.txt", "\t", escape_double = FALSE, trim_ws = TRUE) %>%
       dplyr::select(Id, Male, Female, log2FoldChange, pvalue, padj) %>% 
       mutate(ageBin='17-19')
   )
 
 right_join(gene_info, fittedBias) %>% 
-  write_tsv("../Shiny/GENEX-FB1/Data/fitted.txt")
+  write_tsv("Shiny/GENEX-FB1/Data/fitted.txt")
