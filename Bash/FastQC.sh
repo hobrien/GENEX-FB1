2177a6a9897fc98f9c2a78bdb22f0037a0d31dcb
Heath O'Brien <heath.obrien@gmail.com>
2017-06-29 13:49:43 +0100

    move system-specific info about file locations to arguments at the start of scripts

--- Bash/FastQC.sh
+++ Bash/FastQC.sh
@@ -4,6 +4,9 @@
 #$ -j y
 #$ -S /bin/bash
 #
+
+BASEDIR=/c8000xd3/rnaseq-heath/GENEX-FB1
+
 echo "running fastQC on $@"
-~/src/FastQC/fastqc --outdir=/c8000xd3/rnaseq-heath/GENEX-FB1/FastQC $@
+~/src/FastQC/fastqc --outdir=$BASEDIR/FastQC $@
 echo "finished running fastQC on $@"
--- Bash/Tophat2.sh
+++ Bash/Tophat2.sh
@@ -7,8 +7,9 @@
 #
 
 
+REFDIR=/c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy
 
 tophat --keep-fasta-order --library-type fr-secondstrand --mate-inner-dist 500 --mate-std-dev 50 --num-threads 8 \
-      --transcriptome-index /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.inx \
+      --transcriptome-index $REFDIR/Annotation/Genes.gencode/genes.inx \
       --output-dir $3 \
-      /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/Bowtie2Index/genome $1 $2
+      $REFDIR/Sequence/Bowtie2Index/genome $1 $2
