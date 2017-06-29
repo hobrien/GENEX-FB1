765839d687dde3339eb4051ae3d7f6e1657775e5
Heath O'Brien <heath.obrien@gmail.com>
2017-06-29 18:01:53 +0100

    run all analyses with ReadLength as batch effect
    Sees that the problem was with RIN being treated as a character
    variable rather than with the batch effect

--- Bash/RunEdgeR.sh
+++ Bash/RunEdgeR.sh
@@ -5,12 +5,12 @@
 #$ -S /bin/bash
 
 
-#Rscript FvsMedgeR.r --min 12 --max 20 -a
+Rscript FvsMedgeR.r --min 12 --max 20 -a
 Rscript R/FvsMedgeR.r --min 12 --max 13
 Rscript R/FvsMedgeR.r --min 13 --max 14
 Rscript R/FvsMedgeR.r --min 14 --max 15
-Rscript R/FvsMedgeR.r --min 15 --max 17 -b Sequencer
-Rscript R/FvsMedgeR.r --min 17 --max 20 -b Sequencer
+Rscript R/FvsMedgeR.r --min 15 --max 17
+Rscript R/FvsMedgeR.r --min 17 --max 20
 
 # Rscript FvsMedgeR.r --min 12 --max 20 -a -r 5
 # Rscript FvsMedgeR.r --min 12 --max 13 -r 5
