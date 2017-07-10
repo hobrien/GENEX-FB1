334784837a1867a0459d461e3e6137c68d1da781
hobrien <heath.obrien@gmail.com>
2017-07-10 13:20:22 +0100

    changed extension name (I guess this didn't matter on mac becasue the file system is not case-sensitive)

--- Bash/RunEdgeR.sh
+++ Bash/RunEdgeR.sh
@@ -5,30 +5,30 @@
 #$ -S /bin/bash
 
 
-Rscript R/FvsMedgeR.r --min 12 --max 20 --cofactor PCW
-Rscript R/FvsMedgeR.r --min 12 --max 20 --cofactor PCW --exclude 17046,18655,11971
-Rscript R/FvsMedgeR.r --min 12 --max 13
-Rscript R/FvsMedgeR.r --min 12 --max 13 --exclude 17046
-Rscript R/FvsMedgeR.r --min 13 --max 14
-Rscript R/FvsMedgeR.r --min 13 --max 14 --exclude 18655  
-Rscript R/FvsMedgeR.r --min 14 --max 15
-Rscript R/FvsMedgeR.r --min 15 --max 17
-Rscript R/FvsMedgeR.r --min 17 --max 20
-Rscript R/FvsMedgeR.r --min 17 --max 20 --exclude 11971
+Rscript R/FvsMedgeR.R --min 12 --max 20 --cofactor PCW
+Rscript R/FvsMedgeR.R --min 12 --max 20 --cofactor PCW --exclude 17046,18655,11971
+Rscript R/FvsMedgeR.R --min 12 --max 13
+Rscript R/FvsMedgeR.R --min 12 --max 13 --exclude 17046
+Rscript R/FvsMedgeR.R --min 13 --max 14
+Rscript R/FvsMedgeR.R --min 13 --max 14 --exclude 18655  
+Rscript R/FvsMedgeR.R --min 14 --max 15
+Rscript R/FvsMedgeR.R --min 15 --max 17
+Rscript R/FvsMedgeR.R --min 17 --max 20
+Rscript R/FvsMedgeR.R --min 17 --max 20 --exclude 11971
 
-# Rscript R/FvsMedgeR.r --min 12 --max 20 -cofactor PCW -r 5
-# Rscript R/FvsMedgeR.r --min 12 --max 13 -r 5
-# Rscript R/FvsMedgeR.r --min 13 --max 14 -r 5
-# Rscript R/FvsMedgeR.r --min 14 --max 15 -r 5
-# Rscript R/FvsMedgeR.r --min 15 --max 17 -r 5
-# Rscript R/FvsMedgeR.r --min 17 --max 20 -r 5
+# Rscript R/FvsMedgeR.R --min 12 --max 20 -cofactor PCW -r 5
+# Rscript R/FvsMedgeR.R --min 12 --max 13 -r 5
+# Rscript R/FvsMedgeR.R --min 13 --max 14 -r 5
+# Rscript R/FvsMedgeR.R --min 14 --max 15 -r 5
+# Rscript R/FvsMedgeR.R --min 15 --max 17 -r 5
+# Rscript R/FvsMedgeR.R --min 17 --max 20 -r 5
 
-Rscript R/FvsMedgeR.r --min 12 --max 20 --cofactor PCW --tool DESeq
-Rscript R/FvsMedgeR.r --min 12 --max 13 --tool DESeq
-Rscript R/FvsMedgeR.r --min 13 --max 14 --tool DESeq
-Rscript R/FvsMedgeR.r --min 14 --max 15 --tool DESeq
-Rscript R/FvsMedgeR.r --min 15 --max 17 --tool DESeq
-Rscript R/FvsMedgeR.r --min 17 --max 20 --tool DESeq
+Rscript R/FvsMedgeR.R --min 12 --max 20 --cofactor PCW --tool DESeq
+Rscript R/FvsMedgeR.R --min 12 --max 13 --tool DESeq
+Rscript R/FvsMedgeR.R --min 13 --max 14 --tool DESeq
+Rscript R/FvsMedgeR.R --min 14 --max 15 --tool DESeq
+Rscript R/FvsMedgeR.R --min 15 --max 17 --tool DESeq
+Rscript R/FvsMedgeR.R --min 17 --max 20 --tool DESeq
 
 Rscript R/FvsMedgeR.R --min 12 --max 20 --interaction PCW --tool DESeqLRT
 Rscript R/FvsMedgeR.R --min 12 --max 20 --cofactor PCW --tool DESeqLRT
