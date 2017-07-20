058d34dd03d385515f8a136441e29718c2de0071
Heath O'Brien <heath.obrien@gmail.com>
2017-07-20 21:28:35 +0100

    added file test to avoid redoing counts

--- Bash/dexseq-count.sh
+++ Bash/dexseq-count.sh
@@ -3,20 +3,30 @@
 #$ -cwd
 #$ -j y
 #$ -S /bin/bash
+#$ -l h_vmem=8G
 #
 
-export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
-
-# see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
 filename=${1##*/}
 filename=${filename%%.bam}
 sampleID=${filename%%.*}
 
-echo "Getting counts for $filename"
+if [ ! -f DEXSeq/$sampleID/$filename.dex_counts.txt ]
+then
+    echo "Getting counts for $filename"
+    mkdir DEXSeq/$sampleID/
+    dexseq_count.py -p yes -r name -s reverse -f bam \
+       /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.gff \
+      $1 \
+      DEXSeq/$sampleID/$filename.dex_counts.txt
+    if [ $? -eq 0 ]
+    then
+        echo "Finished getting counts for $filename"
+    else
+        echo "Could not get counts for $filename"
+        exit 1
+    fi    
+fi
 
-dexseq_count.py -p yes -r name -s reverse -f bam \
-  /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.gff \
-  $1 \
-  /c8000xd3/rnaseq-heath/GENEX-FB1/DEXSeq/$sampleID/BAM/$filename.dex_counts.txt
-echo "Finished getting counts for $filename"
+
+
