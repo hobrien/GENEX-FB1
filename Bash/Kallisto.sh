f754e25f30e2aad1704f99d46ffb0e8e8ee6f9c6
hobrien <heath.obrien@gmail.com>
2017-07-26 10:16:01 +0100

    run Kallisto quantification and bootstrapping

--- Bash/Kallisto.sh
+++ Bash/Kallisto.sh
@@ -3,13 +3,20 @@
 #$ -cwd
 #$ -j y
 #$ -S /bin/bash
+#$ -l h_vmem=12G
 #
+
+filename=${1##*/}
+filename=${filename%%.bam}
+sampleID=${filename%%.*}
+
+export PATH=$PATH:/share/apps/R-3.2.2/bin:/share/apps/
 
 REFDIR=/c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy
 
-if [ ! -f $REFDIR//Annotation/Genes.gencode/genes.fa ]
+if [ ! -f $REFDIR/Annotation/Genes.gencode/genes.fa ]
 then
-    echo "Creating directory extracting transcript sequences"
+    echo "Extracting transcript sequences"
     cut -f 1-12 \
       $REFDIR/Annotation/Genes.gencode/genes.bed > /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.bed12
 
@@ -18,8 +25,8 @@ then
       -bed $REFDIR/Annotation/Genes.gencode/genes.bed12 \
       -split -name -s \
       | fold -w 60 \
-      | perl -pe 's/\([+-]\)// \
-      > $REFDIR//Annotation/Genes.gencode/genes.fa
+      | perl -pe 's/\([+-]\)//' \
+      > $REFDIR/Annotation/Genes.gencode/genes.fa
     if [ $? -eq 0 ]
     then
         echo "Finished extracting transcript sequences"
@@ -29,4 +36,33 @@ then
     fi    
 fi
 
-kallisto index -i $REFDIR/Annotation/Genes.gencode/genes.fa
+if [ ! -f $REFDIR/Annotation/Genes.gencode/kallisto.inx ]
+then
+    echo "Creating kallisto index"
+    kallisto index -i $REFDIR/Annotation/Genes.gencode/kallisto.inx $REFDIR/Annotation/Genes.gencode/genes.fa
+    if [ $? -eq 0 ]
+    then
+        echo "Finished creating kallisto index"
+    else
+        echo "Could not create kallisto index"
+        exit 1
+    fi    
+fi
+
+if [ ! -f Kallisto/$SampleID/abundances.h5 ] | [ ! -f Kallisto/$SampleID/abundances.tsv ] | [ ! -f Kallisto/$SampleID/run_info.json ]
+then
+    echo "Running Kallisto on $SampleID"
+    kallisto quant -i $REFDIR/Annotation/Genes.gencode/kallisto.inx \
+      -o Kallisto/$SampleID \
+      --bias \
+      -b 100 \
+      --rf-stranded \
+      $@
+    if [ $? -eq 0 ]
+    then
+        echo "Finished running Kallisto on $SampleID"
+    else
+        echo "Could not run Kallisto on $SampleID"
+        exit 1
+    fi    
+fi
