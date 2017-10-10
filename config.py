75949a9249d3dd064167fe0e3a859c910d363b3b
Heath O'Brien <heath.obrien@gmail.com>
2017-10-10 15:06:10 +0100

    need to look up file names using prefixes in sequence file

--- config.py
+++ config.py
@@ -1,14 +1,25 @@
 import csv
 import os
+import re
 from collections import defaultdict
 
 
 with open("Data/sequences.txt", "r") as f:
     reader = csv.reader(f, delimiter="\t")
-    sequence_folders = defaultdict(list)
+    samples = defaultdict(list)
+    filenames = defaultdict(list)
+    folders = defaultdict(list)
     for row in reader:
-        sequence_folders[row[1].split('-')[0]].append(row[4])
-    for sample in sequence_folders.keys():
-        for folder in set(sequence_folders[seq]):
-            print(os.listdir(folder))
-        
+        sample = row[1]
+        brain_bank_id = sample.split('-')[0]
+        samples[brain_bank_id].append(sample)
+        folders[sample].append(row[4])
+        filenames[sample].append(row[0])
+    for brain_bank_id in samples.keys():
+        sequences = []
+        for sample in set(samples[brain_bank_id]):
+            for read in range(2):
+                folder = folders[sample][read]
+                file = filenames[sample][read]
+                sequences = sequences + [f for f in os.listdir(folder) if re.match(re.escape(file) + r'.*f(ast)?q(.gz)?$', f)]
+        print(": ".join([brain_bank_id, ",".join(sorted(sequences))]))
